aa5c8749af91b5e4a6e587d697273f25263c1f58
sarah2056 <saravasquezvasquez16@gmail.com>
2022-09-02 13:27:16 -0500

    segundo cambio

---
+++ JS/torus.js
@@ -0,0 +1,32 @@
+//torus
+//escena
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x002232)
+
+scene.fog = new THREE.Fog( 0x000000, 3, 100);
+//camara
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+
+//renderizado
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+//gemetria
+const geometry = new THREE.TorusGeometry( 8, 2 ,8 , 5 );
+const material = new THREE.MeshBasicMaterial( { color: 0x135878} );
+const torus = new THREE.Mesh( geometry, material );
+scene.add( torus );
+
+camera.position.z = 20;
+
+
+//funcion
+function animate() {
+    torus.rotation.x += 0.01;
+    torus.rotation.y += 0.01;
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+}
+animate();
